2d913e5c952cc336408fbb94a76d69e5e691164d
payalkherajani <payal.kherajani19@gmail.com>
2021-07-13 21:46:01 +0530

    backend:mongodb-connected

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express, { Express } from 'express';
 
 const app: Express = express();
+import connectDB from './config/db';
 
-
+connectDB();
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`Server Started on PORT ${PORT}`);
 });
