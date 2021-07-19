f53ef15ef1b9fee356327f87ebdc9286d46705e9
payalkherajani <payal.kherajani19@gmail.com>
2021-07-19 21:33:08 +0530

    backend:sign-in-api-without-google

--- backend/src/controllers/users.controller.ts
+++ backend/src/controllers/users.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import User, { UserFieldsInterface } from "../models/users.model";
 import bcrypt from 'bcrypt';
 import gravatar from 'gravatar';
+import { generateToken } from "../utils/token";
 
 const signUpWithoutGoogleAPI = async (req: Request, res: Response) => {
     try {
@@ -29,4 +30,26 @@ const signUpWithoutGoogleAPI = async (req: Request, res: Response) => {
         return res.status(500).json({ success: false, message: 'Server Error' });
     }
 };
-export default { signUpWithoutGoogleAPI };
+
+const signInWithoutSignIn = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body!;
+        const user = await User.findOne({ email });
+        if (user) {
+            const isPasswordMatch = await bcrypt.compare(password, user.password);
+            if (isPasswordMatch) {
+                const data = { id: user._id };
+                const token = generateToken(data);
+                return res.status(200).json({ success: true, token });
+            }
+            else {
+                return res.status(400).json({ success: false, message: 'Invalid Credentials' });
+            }
+        }
+
+    } catch (err) {
+        return res.status(500).json({ success: false, message: 'Server Error' });
+    }
+};
+
+export default { signUpWithoutGoogleAPI, signInWithoutSignIn };
