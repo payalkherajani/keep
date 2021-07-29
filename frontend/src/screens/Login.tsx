dc480d2edb7d30ee08f557812f0906549f7592e1
payalkherajani <payal.kherajani19@gmail.com>
2021-07-29 17:03:01 +0530

    frontend:added-tailwind-forms

--- frontend/src/screens/Login.tsx
+++ frontend/src/screens/Login.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+
 const Login = () => {
     return (
         <div className="container">
-            <div className="bg-login-background min-h-screen max-w-full"></div>
+            <div className="bg-login-background min-h-screen">
+                <div className="container mx-auto px-4">
+
+                    <input type="email"
+                        placeholder="email"
+                        className="form-input px-4 py-3 rounded-full"
+                    />
+
+                    <input
+                        type="password"
+                        placeholder="password"
+                        className="form-input px-4 py-3 rounded-full"
+                    />
+
+                    <button>Login</button>
+                </div>
+            </div>
         </div>
     );
 };
