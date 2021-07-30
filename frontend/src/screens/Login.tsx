788b18a1a4e4aaf010159abca8c4937881cbd876
payalkherajani <payal.kherajani19@gmail.com>
2021-07-30 11:25:23 +0530

    frontend:color-changed-to-purple

--- frontend/src/screens/Login.tsx
+++ frontend/src/screens/Login.tsx
@@ -5,19 +5,19 @@ const Login = () => {
         <div className="container">
             <div className="bg-login-background min-h-screen">
                 <div className="container shadow-inner mx-auto p-4 md:w-2/4">
-                    <h1 className="mb-8 text-center"><strong className="text-pink-400">LOGIN</strong></h1>
+                    <h1 className="mb-8 text-center"><strong className="text-purple-400">LOGIN</strong></h1>
                     <form className="flex flex-col">
                         <input
                             type="email"
                             placeholder="email"
-                            className="px-4 py-3 mb-8 text-pink-500  border-0 border-b-2 border-pink-200 focus:border-pink-600 hover:border-pink-300 focus:outline-none"
+                            className="px-4 py-3 mb-8 text-purple-500  border-0 border-b-2 border-purple-200 focus:border-purple-600 hover:border-purple-300 focus:outline-none"
                         />
                         <input
                             type="password"
                             placeholder="password"
-                            className="px-4 py-3 mb-8 text-pink-500  border-0 border-b-2 border-pink-200 hover:border-pink-300 focus:border-pink-600 focus:outline-none"
+                            className="px-4 py-3 mb-8 text-purple-500  border-0 border-b-2 border-purple-200 hover:border-purple-300 focus:border-purple-600 focus:outline-none"
                         />
-                        <button className="text-white py-2 px-4 font-semibold rounded-lg bg-pink-600 hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-600 focus:ring-opacity-50">Login</button>
+                        <button className="text-white py-2 px-4 font-semibold rounded-lg bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50">Login</button>
                     </form>
 
                 </div>
