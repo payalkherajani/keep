3a38eb3b104c9bb5bad105ee9f3a726e3a05f75d
payalkherajani <payal.kherajani19@gmail.com>
2021-08-08 14:43:38 +0530

    frontend:float-button-made

--- frontend/src/screens/Notes.tsx
+++ frontend/src/screens/Notes.tsx
@@ -6,6 +6,7 @@ const Notes = () => {
         <div className="container" style={{ maxWidth: '100%', margin: '0rem' }}>
             <Navbar />
             <NotesCard />
+            <button className="fixed right-5 h-24 w-24 bottom-10 z-10 rounded-full p-5 bg-purple-400"><i className="fas fa-plus text-2xl text-white"></i></button>
         </div>
     );
 };
