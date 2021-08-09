f51fd3003a524c9f488efb14f7c5ffba93183182
payalkherajani <payal.kherajani19@gmail.com>
2021-08-09 21:22:57 +0530

    frontend:modal-add-designed

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const Modal = () => {
+const Modal = (props: { showModal: any; }) => {
     return (
-        <div>
-            Modal
+        <div className="flex flex-col border-2 border-black absolute top-60 left-28 z-10">
+            <input type="text" name="note_title" placeholder="Enter Title" />
+            <input type="text" name="note_description" placeholder="Enter Description" />
+            <select>
+                <option>IMPORTANT</option>
+                <option>CASUAL</option>
+            </select>
+            <button>
+                Save
+            </button>
+            <button>
+                Close
+            </button>
         </div>
     );
 };
--- frontend/src/screens/Notes.tsx
+++ frontend/src/screens/Notes.tsx
@@ -7,9 +7,15 @@ const Notes = () => {
         <div className="container" style={{ maxWidth: '100%', margin: '0rem' }}>
             <Navbar />
             <NotesCard />
-            <button className="fixed right-5 h-20 w-20 bottom-10 z-10 rounded-full p-5 bg-purple-400"><i className="fas fa-plus text-2xl text-white"></i></button>
+            <button
+                className="fixed right-5 h-20 w-20 bottom-10 z-10 rounded-full p-5 bg-purple-400"
+                onClick={() => setShowModal(true)}
+
+            >
+                <i className="fas fa-plus text-2xl text-white"></i>
+            </button>
             {
-                showModal && <Modal />
+                showModal && <Modal showModal={setShowModal} />
             }
         </div>
     );
