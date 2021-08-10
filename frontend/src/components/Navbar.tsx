0967a5590d0e48d64a317d86ca3f06e868ebef4d
payalkherajani <payal.kherajani19@gmail.com>
2021-08-10 22:43:17 +0530

    frontend:avatar-added

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import avatar from '../assest/avatar1.jpg';
 
 const Navbar = () => {
     return (
-        <div className="container bg-purple-400 py-8 flex md:px-8 text-white flex justify-between" style={{ maxWidth: '100%' }}>
+        <div className="container items-center bg-purple-400 py-8 flex md:px-8 text-white flex justify-between" style={{ maxWidth: '100%' }}>
             <h1><strong>SMRITI</strong></h1>
-            <div><button><i className="fas fa-sign-out-alt text-2xl"></i></button></div>
+            <div className="flex gap-2">
+                <img src={avatar} alt="avatar" className="avatar avatar-md" />
+                <button><i className="fas fa-sign-out-alt text-2xl"></i></button>
+            </div>
+
         </div>
     );
 };
