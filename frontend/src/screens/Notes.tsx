070e8d1501f6000287f599f93f912dfcc21c3e3a
payalkherajani <payal.kherajani19@gmail.com>
2021-08-10 11:19:29 +0530

    frontend:opacity-done

--- frontend/src/screens/Notes.tsx
+++ frontend/src/screens/Notes.tsx
@@ -6,7 +6,7 @@ const Notes = () => {
     return (
         <div className="container" style={{ maxWidth: '100%', margin: '0rem' }}>
             <Navbar />
-            <NotesCard />
+            <NotesCard showModal={showModal} />
             <button
                 className="fixed right-5 h-20 w-20 bottom-10 z-10 rounded-full p-5 bg-purple-400"
                 onClick={() => setShowModal(true)}
