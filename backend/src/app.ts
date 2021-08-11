3815d2f636d54672a604ed1bdd7d830a61fbeef9
payalkherajani <payal.kherajani19@gmail.com>
2021-08-11 20:58:07 +0530

    frontend:fixed-backend-urls

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import userRoutes from './routes/users.route';
 import notesRoutes from './routes/notes.route';
 import NoSuchRouteExists from './middlewares/noRoute.middleware';
 import auth from './middlewares/auth.middleware';
-
+import cors from 'cors';
 
 const app: Application = express();
 
@@ -13,6 +13,7 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/users', userRoutes);
 app.use('/notes', auth, notesRoutes);
