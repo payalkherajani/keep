9faf3ec3b8d401b8e31f662e4931f7312b07aad3
payalkherajani <payal.kherajani19@gmail.com>
2021-08-12 11:00:57 +0530

    frontend:added-constants

--- frontend/src/constants/Constants.ts
+++ frontend/src/constants/Constants.ts
@@ -1,1 +1,5 @@
-export const GET_ALL_NOTES = 'GET_ALL_NOTES'
+export const GET_ALL_NOTES = 'GET_ALL_NOTES';
+export const GET_USER_DETAILS = 'GET_USER_DETAILS';
+export const UPDATE_NOTE = 'UPDATE_NOTE';
+export const FETCH_NOTE_DETAILS = 'FETCH_NOTE_DETAILS';
+export const DELETE_NOTE = 'DELETE_NOTE';
